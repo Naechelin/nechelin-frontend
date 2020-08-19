08d7a7e57277f9bd324f40ec4cc5de8e0b7ad849
jeonka1001 <ruddks2009@naver.com>
2020-08-19 16:25:01 +0900

    map.js 에 버튼 추가 프리즈

--- nechelin/src/Components/Map/Map/Map.js
+++ nechelin/src/Components/Map/Map/Map.js
@@ -15,8 +15,38 @@ class Map extends PureComponent {
 					center: new kakao.maps.LatLng(33.450701, 126.570667),
 				};
 				window.map = new kakao.maps.Map(container, options);
+
+				function setMapType(maptype) {
+					var roadmapControl = document.getElementById('btnRoadmap');
+					var skyviewControl = document.getElementById('btnSkyview');
+					if (maptype === 'roadmap') {
+						window.map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+						roadmapControl.className = 'selected_btn';
+						skyviewControl.className = 'btn';
+					} else {
+						window.map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+						skyviewControl.className = 'selected_btn';
+						roadmapControl.className = 'btn';
+					}
+				}
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+				function zoomIn() {
+					window.map.setLevel(window.map.getLevel() - 1);
+				}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+				function zoomOut() {
+					window.map.setLevel(window.map.getLevel() + 1);
+				}
+
 			});
+
+
+
 		});
+
+
 	}
 	render() {
 		return (
