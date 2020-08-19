3d3cf84804207573715d830b65233f80739a4b1e
jeonka1001 <ruddks2009@naver.com>
2020-08-19 16:09:00 +0900

    store / map

--- nechelin/src/Components/Map/Map/Map.js
+++ nechelin/src/Components/Map/Map/Map.js
@@ -3,10 +3,24 @@ import "./Map.css";
 import SearchBar from "../../Common/SearchBar/SearchBar";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar, faBook} from "@fortawesome/free-solid-svg-icons";
-
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+const {kakao}=window;
 class Map extends PureComponent {
+	componentDidMount() {
+		window.$(document).ready(function () {
+			kakao.maps.load(()=>{
+				let container = document.getElementById("map");
+				let options = {
+					center: new kakao.maps.LatLng(33.450701, 126.570667),
+				};
+				window.map = new kakao.maps.Map(container, options);
+			});
+		});
+	}
 	render() {
 		return (
+
 			<div className="Map">
 				<select>
 					<option>내위치</option>
@@ -20,7 +34,7 @@ class Map extends PureComponent {
 						alt="길찾기"
 					></img>
 				</button>
-				<div>map</div>
+				<div id="map"></div>
 				<div className="filter">
 					<FontAwesomeIcon icon={faStar} color="yellow" />
 					<FontAwesomeIcon icon={faStar} color="red" />
