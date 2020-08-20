205ea28394f702ba4fe89925bb41d8ad51921f24
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-08-20 15:19:39 +0900

    끝

--- nechelin/src/Components/Owner/Chart/Chart.js
+++ nechelin/src/Components/Owner/Chart/Chart.js
@@ -1,8 +1,31 @@
 import React, {PureComponent} from "react";
-
+import ChartComponent from "./ChartComponent";
 class Chart extends PureComponent {
 	render() {
-		return <div>Chart</div>;
+		return (
+			<div className="Chart">
+				<div className="chartDiv">
+					<label id="chartTitle">분석차트</label>
+				</div>
+				<div className="stampHistory">
+					<label id="stampTitle">스탬프 내역</label>
+					<ul>
+						<li>
+							<ChartComponent />
+						</li>
+						<li>
+							<ChartComponent />
+						</li>
+						<li>
+							<ChartComponent />
+						</li>
+						<li>
+							<ChartComponent />
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
 	}
 }
 
